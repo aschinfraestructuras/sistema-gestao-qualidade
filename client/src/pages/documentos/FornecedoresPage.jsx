000eb485301966a7eb1049a5a6d6a531b35f9bb2
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-12 15:29:02 +0100

    Create FornecedoresPage.jsx

---
+++ client/src/pages/documentos/FornecedoresPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FornecedoresPage = () => {
+  return (
+    <div>
+      <h1>Fornecedores</h1>
+      <p>Página em desenvolvimento para registo e gestão de fornecedores.</p>
+    </div>
+  );
+};
+
+export default FornecedoresPage;
