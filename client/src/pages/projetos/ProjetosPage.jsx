85a07c81bef37258a3cdd0054c056aa4eba8ea82
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-12 15:32:22 +0100

    Create ProjetosPage.jsx

---
+++ client/src/pages/projetos/ProjetosPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjetosPage = () => {
+  return (
+    <div>
+      <h1>Projetos</h1>
+      <p>Página em desenvolvimento para dados dos projetos em curso.</p>
+    </div>
+  );
+};
+
+export default ProjetosPage;
