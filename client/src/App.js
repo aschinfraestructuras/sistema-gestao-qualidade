3eaa9d0639d64c3552a491f0b2ed3bb049c123fd
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-12 12:16:35 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
-// client/src/App.js
-
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-
 // Páginas
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
-
 // Páginas de Módulos
 import DocumentosPage from './pages/documentos/DocumentosPage';
 import EnsaiosPage from './pages/ensaios/EnsaiosPage';
@@ -22,14 +18,11 @@ import ChecklistsPage from './pages/checklists/ChecklistsPage';
 import FornecedoresPage from './pages/fornecedores/FornecedoresPage';
 import MateriaisPage from './pages/materiais/MateriaisPage';
 import ProjetosPage from './pages/projetos/ProjetosPage';
-
 // Context
 import { AuthProvider } from './context/AuthContext';
-
 // Componentes
 import Layout from './components/layout/Layout';
 import ProtectedRoute from './components/auth/ProtectedRoute';
-
 // Tema da aplicação
 const theme = createTheme({
   palette: {
@@ -73,48 +66,50 @@ const theme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          
-          {/* Rotas protegidas */}
-          <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
             
-            {/* Módulo de Projetos */}
-            <Route path="/projetos" element={<ProjetosPage />} />
+            {/* Rotas protegidas */}
+            <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              
+              {/* Módulo de Projetos */}
+              <Route path="/projetos" element={<ProjetosPage />} />
+              
+              {/* Módulo de Documentos */}
+              <Route path="/documentos" element={<DocumentosPage />} />
+              
+              {/* Módulo de Ensaios */}
+              <Route path="/ensaios" element={<EnsaiosPage />} />
+              <Route path="/ensaios/dashboard" element={<EnsaioDashboardPage />} />
+              <Route path="/ensaios/adicionar" element={<AddEnsaioPage />} />
+              <Route path="/ensaios/editar/:id" element={<EditEnsaioPage />} />
+              <Route path="/ensaios/:id" element={<EnsaioDetailsPage />} />
+              
+              {/* Módulo de Não Conformidades */}
+              <Route path="/nao-conformidades" element={<NCPage />} />
+              
+              {/* Módulo de Checklists */}
+              <Route path="/checklists" element={<ChecklistsPage />} />
+              
+              {/* Módulo de Fornecedores */}
+              <Route path="/fornecedores" element={<FornecedoresPage />} />
+              
+              {/* Módulo de Materiais */}
+              <Route path="/materiais" element={<MateriaisPage />} />
+            </Route>
             
-            {/* Módulo de Documentos */}
-            <Route path="/documentos" element={<DocumentosPage />} />
-            
-            {/* Módulo de Ensaios */}
-            <Route path="/ensaios" element={<EnsaiosPage />} />
-            <Route path="/ensaios/dashboard" element={<EnsaioDashboardPage />} />
-            <Route path="/ensaios/adicionar" element={<AddEnsaioPage />} />
-            <Route path="/ensaios/editar/:id" element={<EditEnsaioPage />} />
-            <Route path="/ensaios/:id" element={<EnsaioDetailsPage />} />
-            
-            {/* Módulo de Não Conformidades */}
-            <Route path="/nao-conformidades" element={<NCPage />} />
-            
-            {/* Módulo de Checklists */}
-            <Route path="/checklists" element={<ChecklistsPage />} />
-            
-            {/* Módulo de Fornecedores */}
-            <Route path="/fornecedores" element={<FornecedoresPage />} />
-            
-            {/* Módulo de Materiais */}
-            <Route path="/materiais" element={<MateriaisPage />} />
-          </Route>
-          
-          {/* Rota 404 */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </AuthProvider>
-    </ThemeProvider>
+            {/* Rota 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
