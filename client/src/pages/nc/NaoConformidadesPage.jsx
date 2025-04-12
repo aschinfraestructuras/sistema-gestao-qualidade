4a9aef1e084c18541c8f35d36a271bfa83601069
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-12 15:31:51 +0100

    Create NaoConformidadesPage.jsx

---
+++ client/src/pages/nc/NaoConformidadesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NaoConformidadesPage = () => {
+  return (
+    <div>
+      <h1>Não Conformidades</h1>
+      <p>Página em desenvolvimento para gestão de NC e ações corretivas.</p>
+    </div>
+  );
+};
+
+export default NaoConformidadesPage;
