aa723bdab12009cb1e7b959caac0f9c41a0dd1ea
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-12 15:27:52 +0100

    Create DocumentosPage.jsx

---
+++ client/src/pages/documentos/DocumentosPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DocumentosPage = () => {
+  return (
+    <div>
+      <h1>Documentos</h1>
+      <p>Página em desenvolvimento para gestão de documentos da obra.</p>
+    </div>
+  );
+};
+
+export default DocumentosPage;
