f4378ac650132186b3666bd2c6541ebc0680be90
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-12 15:32:51 +0100

    Create ChecklistsPage.jsx

---
+++ client/src/pages/checklists/ChecklistsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChecklistsPage = () => {
+  return (
+    <div>
+      <h1>Checklists</h1>
+      <p>Página em desenvolvimento para planeamento e verificação de tarefas.</p>
+    </div>
+  );
+};
+
+export default ChecklistsPage;
