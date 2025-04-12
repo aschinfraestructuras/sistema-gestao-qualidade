37105b496291689e35462327e20781e47aac0341
aschinfraestructuras <quim_leiria@hotmail.com>
2025-04-12 15:31:09 +0100

    Create MateriaisPage.jsx

---
+++ client/src/pages/materiais/MateriaisPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MateriaisPage = () => {
+  return (
+    <div>
+      <h1>Materiais</h1>
+      <p>Página em desenvolvimento para rastreio de materiais utilizados.</p>
+    </div>
+  );
+};
+
+export default MateriaisPage;
